7587bce7490a762f77e6ec603738a193208d227b
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-04 21:11:26 +0300

    print fun. code

--- assignment 05/Program.cs
+++ assignment 05/Program.cs
@@ -7,11 +7,19 @@ namespace assignment_05
 {
     internal class Program
     {
-
+        public static void printshape(int count = 10 , string pattern = "*_*") 
+        {
+        for (int i = 0; i < count; i++) {
+                Console.WriteLine(pattern);
+            }
+        
+        
+        }
         static void Main(string[] args)
         {
- 
-
+            #region print fun. code
+            //printshape(2,":)"); 
+            #endregion
         }
     }
 }
