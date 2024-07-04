ec6ab265eeca53a3afc4b338c03e911543f2cc3d
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-04 21:22:05 +0300

    value type parameters passing value

--- assignment 05/Program.cs
+++ assignment 05/Program.cs
@@ -7,19 +7,34 @@ namespace assignment_05
 {
     internal class Program
     {
-        public static void printshape(int count = 10 , string pattern = "*_*") 
-        {
-        for (int i = 0; i < count; i++) {
-                Console.WriteLine(pattern);
-            }
+        //public static void printshape(int count = 10 , string pattern = "*_*") 
+        //{
+        //for (int i = 0; i < count; i++) {
+        //        Console.WriteLine(pattern);
+        //    }
+        //}
+        //static void swap(int x, int y) {
+        //    int temp = x;
+        //    x = y; 
+        //    y = temp;    
         
-        
-        }
+        //}
         static void Main(string[] args)
         {
             #region print fun. code
             //printshape(2,":)"); 
             #endregion
+            #region passing Value type parameters
+            //int a = 10, b = 20;
+            //Console.WriteLine($"a = {a} \n b = {b}");
+            //swap(a, b);
+            //Console.WriteLine("swap");
+            //Console.WriteLine($"a = {a} \n b = {b}");
+
+
+            #endregion
+
+
         }
     }
 }
